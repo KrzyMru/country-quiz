695dde744ce3af3317cc159cb904ce42d3eb169c
KrzyMru <krzymru20@gmail.com>
2025-10-14 14:56:38 +0200

    Quiz styling

--- src/app/components/quiz/quiz.tsx
+++ src/app/components/quiz/quiz.tsx
@@ -1,12 +1,15 @@
+import "./quiz.css";
 import Question from "../question/question";
 
 const Quiz = () => {
     return (
-        <div>
-            <ul>
+        <div className="quiz__container">
+            <ul className="quiz__questions">
             {
                 Array.from({ length: 10 }, (_, i) => (
-                    <span>{i}</span>
+                    <li key={i}>
+                        <span className="quiz__question">{i+1}</span>
+                    </li>
                 ))
             }
             </ul>
