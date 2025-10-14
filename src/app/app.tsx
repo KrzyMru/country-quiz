4958a27331d43ac54369b09143a6262c74253e6f
KrzyMru <krzymru20@gmail.com>
2025-10-14 15:06:12 +0200

    Small positioning fix

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,11 +4,13 @@ import Quiz from "./components/quiz/quiz";
 const App = () => {
   return (
     <main className="page">
-      <div className="header__container">
-        <header className="header__logo">Country Quiz</header>
-        <span className="header__points">4/10 Points</span>
+      <div className="quiz__wrapper">
+        <div className="header__container">
+          <header className="header__logo">Country Quiz</header>
+          <span className="header__points">4/10 Points</span>
+        </div>
+        <Quiz />
       </div>
-      <Quiz />
     </main>
   )
 }
