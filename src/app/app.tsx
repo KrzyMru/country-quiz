1c38ed77ef392ffc52e963e92e912e6a44f08893
KrzyMru <krzymru20@gmail.com>
2025-10-14 14:43:42 +0200

    Main page styling

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,11 +1,12 @@
+import "./app.css";
 import Quiz from "./components/quiz/quiz";
 
 const App = () => {
   return (
-    <main>
-      <div>
-        <header>Country Quiz</header>
-        <span>4/10 Points</span>
+    <main className="page">
+      <div className="header__container">
+        <header className="header__logo">Country Quiz</header>
+        <span className="header__points">4/10 Points</span>
       </div>
       <Quiz />
     </main>
